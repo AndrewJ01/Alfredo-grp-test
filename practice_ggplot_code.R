f7d8b154e7afabefebdf8c7e43f3c2ee3f0e4a79
Andrew Black <89215873+AndrewJ01@users.noreply.github.com>
2022-11-15 12:48:47 -0500

    Added a ggplot line

--- practice_ggplot_code.R
+++ practice_ggplot_code.R
@@ -20,6 +20,15 @@
 	sample_number <- nrow(input_data)
 	sample_number
 
+# add ggplot
+	data("midwest", package = "ggplot2")  # load the data
+
+	# plot
+	ggplot(midwest, aes(x=area, y=poptotal)) + 
+	  geom_point(col="steelblue", size=3) +   # Set static color and size for points
+	  geom_smooth(method="lm", col="firebrick") +  # change the color of line
+	  coord_cartesian(xlim=c(0, 0.1), ylim=c(0, 1000000)) + 
+	  labs(title="Area Vs Population", subtitle="From midwest dataset", y="Population", x="Area", caption="Midwest Demographics")
 
 
 # Other tips:
